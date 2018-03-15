f8addf21d0cf4570351f39e58d17fc35edca4a93
Sol2018 <37330604+Sol2018@users.noreply.github.com>
2018-03-15 05:16:54 +0200

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -10,73 +10,111 @@ import java.util.Scanner;
 
 public class Server extends Thread
 {
-  private ServerSocket serverSocket;
-  private HashMap<String,ServerClientThread> clients;
+    private ServerSocket serverSocket;
+    private HashMap<String,ServerClientThread> clients;
+    private MultiUsers multiUsers = new MultiUsers(); // *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
 
-  public Server(int port) throws IOException
-  {
-    serverSocket = new ServerSocket(port);
-    clients = new HashMap<String,ServerClientThread>();
-  }
-
-  public void run()
-  {
-    while(true)
+    public Server(int port) throws IOException
     {
-      try
-      {
-        System.out.println("Waiting for client on port " + serverSocket.getLocalPort());
-        Socket clientConnection = serverSocket.accept();
-        //Will make neater later
-        DataInputStream tempInStream = new DataInputStream(clientConnection.getInputStream());
-        String clientName = tempInStream.readUTF();
-        ServerClientThread clientThread = new ServerClientThread(clientName,clientConnection,this);
-        //Will add duplicate name check later
-        clients.put(clientName,clientThread);
-        System.out.println("Registered new user: " + clientName);
-        clientThread.start();
-      }
-      catch(IOException e)
-      {
-        System.err.println("IO Exception!!!");
-        e.printStackTrace();
-        break;
-      }
+        serverSocket = new ServerSocket(port);
+        clients = new HashMap<String,ServerClientThread>();
     }
-  }
-  /*
-  *This is where the file codes should be managed
-  * 0 is message. if target is all treat as broadcast.
-  * 1 is file.
-  */
-  public synchronized void send(Message msg,String sender)
-  {
-    clients.get(msg.getTarget()).sendToSocket(msg,sender);
-  }
 
-  public static void main(String[] args)
-  {
-    int port = Integer.parseInt(args[0]);
-    try
+    public void run()
     {
-      Server s = new Server(port);
-      s.run(); //Intentional
+        while(true)
+        {
+            try
+            {
+                System.out.println("Waiting for client on port " + serverSocket.getLocalPort());
+                Socket clientConnection = serverSocket.accept();
+                //Will make neater later
+                DataInputStream tempInStream = new DataInputStream(clientConnection.getInputStream());
+                String clientName = tempInStream.readUTF();
+                ServerClientThread clientThread = new ServerClientThread(clientName,clientConnection,this);
+                //Will add duplicate name check later
+                clients.put(clientName,clientThread);
+                System.out.println("Registered new user: " + clientName);
+                clientThread.start();
+            }
+            catch(IOException e)
+            {
+                System.err.println("IO Exception!!!");
+                e.printStackTrace();
+                break;
+            }
+        }
     }
-    catch(IOException e)
-    {
-      e.printStackTrace();
-    }
-  }
 
-  //Possibly Temporary
-  //Will add error management later
-  public static Message parseMesseage(String message)
-  {
-    Scanner parser = new Scanner(message).useDelimiter("\\|");
-    Message msg = new Message(parser.nextInt(),parser.next(),parser.next());
-    //System.out.println(msg.toString());
-    parser.close();
-    return msg;
-  }
+    public void send(Message msg,String sender)
+    {
+        switch (msg.getCommand())// *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+        {
+            case 0:
+                if (msg.getTarget().compareTo("all")==0)                                                                //sends update to all clients in the server
+                    new Broadcaster(clients,msg,sender).start();
+                else
+                    clients.get(msg.getTarget()).sendToSocket(msg,sender);                                              //one to one communication without registration
+                break;
+
+            case 1:
+                //file functionality
+                break;
+
+            case 2:
+                multiUsers.registration(msg,sender,clients);                                                                              //registration
+                break;
+
+            case 3:
+                multiUsers.login(msg,clients,sender);
+                break;
+            case 4:
+                multiUsers.logout(msg,clients,sender);
+                break;
+
+            case 5:
+                multiUsers.addFriend(msg,clients,sender);
+                break;
+
+            case 6:
+                multiUsers.createGroup(msg,clients,sender);
+                break;
+            case 7:
+                multiUsers.addMem(msg,clients,sender);
+                break;
+            case 8:
+                multiUsers.printStatus(sender,clients);
+                break;
+            case 9:
+                multiUsers.groupMessage(msg,clients,sender);
+                break;
+        }                       // *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+
+    }
+
+    public static void main(String[] args)
+    {
+        int port = 4444;
+        try
+        {
+            Server s = new Server(port);
+            s.run(); //Intentional
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //Possibly Temporary
+    //Will add error management later
+    public static Message parseMesseage(String message)
+    {
+        Scanner parser = new Scanner(message).useDelimiter("\\|");
+        Message msg = new Message(parser.nextInt(),parser.next(),parser.next());
+        //System.out.println(msg.toString());
+        parser.close();
+        return msg;
+    }
 
 }
